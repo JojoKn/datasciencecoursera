1b8b7d011ce7b19101f5776b9edda785253845f5
JojoKn <h12113366@s.wu.ac.at>
2022-02-25 16:04:19 +0100

    Final chunk of code

--- Coding Task 1.R
+++ Coding Task 1.R
@@ -1,11 +1,17 @@
 getwd()
-pollutantmean<-function(directory, pollutant, id=1:332, na.rm=TRUE){
-  
-  data<-read.csv(directory/id.csv)
-  mean(data$"pollutant", na.rm=TRUE)
-}
-
-pollutantmean("specdata", "sulfate", 332)
 
 X001<-read.csv("specdata/001.csv")
 mean(X001$"nitrate", na.rm=TRUE)
+
+pollutantmean<-function(directory, pollutant, id=1:332, na.rm=TRUE){
+  library(dplyr)
+  fileNames<-paste0(directory, "/", formatC(id, width=3, flag="0"), ".csv")
+  lst<-lapply(fileNames, data.table::fread)
+  dt<-rbindlist(lst)
+  col<-select(dt, pollutant)
+  colMeans(col, na.rm=TRUE)
+}
+
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "nitrate", 23)
